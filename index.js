5d5168d38b35c2e56c9787a510652de2c770a7d0
delm83 <delm83@googlemail.com>
2023-10-26 18:08:39 +0100

    added post function

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
+const validUrl = require('valid-url');
+const nanoId = require('nano-id');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -27,6 +29,24 @@ app.get('/', function(req, res) {
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/shorturl', async (req, res)=>{
+
+  let nano_Id = nanoId();
+  if (!validUrl.isWebUri(req.body.url)) 
+  {res.json({error: 'invalid url'})} 
+  else {
+       let inputURL = await URL.findOne({original_url: req.body.url})
+       if (inputURL) {
+          res.json({original_url: inputURL.original_url, short_url: inputURL.short_url})} 
+       else 
+         {
+          inputURL = new URL({original_url: req.body.url, short_url: nano_Id})
+          await inputURL.save()
+          res.json({original_url: inputURL.original_url, short_url: inputURL.short_url})
+     }
+ }
+});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
