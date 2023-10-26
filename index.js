e5665f91299865df2adde7803baa49c7bdcc1d84
delm83 <delm83@googlemail.com>
2023-10-26 18:04:27 +0100

    connected to mongodb

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,10 +18,7 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
